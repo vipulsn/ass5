a0aa5906a6108b6c10e91faf9870f3d652c7e383
vipulsn <vipulsn@iitk.ac.in>
2020-05-28 02:56:47 +0530

    Adding microkernel function by user_2

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
+void microkernel_sendmsg(char *);
+
 void main(){
 	printf("Helloworld!\n");
+	microkernel_sendmsg("is more portable");
+}
+
+void microkernel_sendmsg(char *a){
+	printf("microkernel: %s\n", a);
 }
