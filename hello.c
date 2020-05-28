258771ce6d231df879ab7015cff1c62a8429840d
vipulsn <vipulsn@iitk.ac.in>
2020-05-28 15:14:56 +0530

    Added feature microkernel_getmsg basic structure

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 void microkernel_sendmsg(char *);
+void microkernel_getmsg(char *);
 
 void main(){
 	printf("Helloworld!\n");
@@ -10,4 +11,8 @@ void main(){
 
 void microkernel_sendmsg(char *a){
 	printf("microkernel: %s\n", a);
+}
+
+void microkernel_getmsg(char *b){
+	//TODO: getmsg feature
 }
