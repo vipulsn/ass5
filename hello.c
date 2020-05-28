d977e2a93dcffaf62a5108b15493395d7acc1a93
vipulsn <vipulsn@iitk.ac.in>
2020-05-28 19:15:38 +0530

    Revert "Erroneous code in hello.c"
    This reverts commit 2a3825b739baebdbdbd496af24347e312171eb72.

--- hello.c
+++ hello.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 
+void microkernel_sendmsg(char *);
+void microkernel_getmsg(char *);
+
 void main(){
 	printf("Helloworld!\n");
 	printf("This must be a monolithic design\n");
@@ -8,4 +11,8 @@ void main(){
 
 void microkernel_sendmsg(char *a){
 	printf("microkernel: %s\n", a);
+}
+
+void microkernel_getmsg(char *b){
+	//TODO: getmsg feature
 }
